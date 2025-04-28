8c743ee55e74bb73fc58e58778f9d6696178ea81
Viru5072353 <virurulo@gmail.com>
2025-04-28 14:33:32 -0300

    P2 realizado

--- P2.java
+++ P2.java
@@ -1,3 +1,35 @@
+/*
+    2. Conversión de temperatura de Celsius a Fahrenheit
+    Desarrollar un algoritmo que tome como entrada una temperatura en grados Celsius y la convierta a grados Fahrenheit utilizando la fórmula:
+    Fahrenheit = (Celsius × 9/5) + 32
+
+    Comprender{
+        pasar de unidad de celcius a fahrenheit
+        Celcius = entrada REAL
+        Fahrenheit = salida REAL
+    }
+
+    Diseñar{
+        Leer Celcius = REAL
+        Fahrenheit = (Celsius × 9/5) + 32 = REAL
+        Mostrar = Fahrenheit
+    }
+*/
+
+import java.util.Scanner;
+
 public class P2 {
-    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Temperatura en Celsius = ");
+
+        Double celsius = sc.nextDouble();
+
+        Double fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.println(celsius + "°C = " + fahrenheit +"°F");
+
+    }
 }
