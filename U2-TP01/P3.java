44c7d0f78d31e53ec67bac760b89da1bc80ae42a
Viru5072353 <virurulo@gmail.com>
2025-04-28 22:10:59 -0300

    ej3

--- U2-TP01/P3.java
+++ U2-TP01/P3.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class P3 {
+  public static void main(String args[]) {
     
+    try (Scanner sc = new Scanner(System.in)) {
+        System.out.println("to end the process please insert 0 as the final product");
+        int i = 1;
+        double sum = 0; 
+        while (i!=0){
+            
+            System.out.print("°"+i+" product: $");
+            
+            double imp = sc.nextDouble();
+            
+            sum = sum + imp;
+            
+            i=i+1;
+            
+            if (imp == 0){
+                i=0;
+            }
+        }
+        System.out.println("Sum: $"+sum);
+    }
+
+    
+  }
 }
